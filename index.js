ddaf4c69562e1c4c69ce2bdab7d25659d009811c
danilowskii <danilopaivatiotonho@gmail.com>
2025-08-18 18:31:35 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const prisma = new PrismaClient();
+app.get("/", async (req, res) => {
+  const getUsers = await prisma.user.findMany();
+  const simplifiedData = getUsers.map((user) => {
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      telefone: user.phonenumber,
+    };
+  });
+  console.log("usuarios retornados: ", simplifiedData);
+  res.json(simplifiedData);
+});
+
+app.post("/api/submit-form", express.json(), async (req, res) => {
+  const formData = await prisma.user.create({
+    data: {
+      name: req.body.name,
+      email: req.body.email,
+      phonenumber: req.body.phonenumber,
+    },
+  });
+
+  res.json({ message: "Form data received sucessfully.", data: formData });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
